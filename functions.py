f1966a7d6777d82ab9299330e012161377d68070
Gennadiy <renardes@gmail.com>
2023-01-07 23:14:49 +0500

    управляющий код

--- functions.py
+++ functions.py
@@ -1,3 +1,30 @@
 """Консольный файловый менеджер.
 
 Дополнительный модуль: вспомогательные функции."""
+
+import data
+
+
+def show_menu():
+    print('\n____ Главное Меню ____')
+    for i, entry in enumerate(data.MainMenu, 1):
+        print(f' {i}. {entry.value}')
+
+
+def get_menu_entry() -> data.MainMenu:
+    while True:
+        inp = input(data.PROMPT)
+        if inp.isdecimal():
+            inp = int(inp)
+            if inp in range(1, len(data.MainMenu)+1):
+                return list(data.MainMenu)[inp-1]
+            else:
+                print(data.ERROR_MESSAGES['RANGE'])
+        else:
+            print(data.ERROR_MESSAGES['DIGITS'])
+
+
+# тесты
+if __name__ == '__main__':
+    show_menu()
+    print(get_menu_entry())
--- main.py
+++ main.py
@@ -1,3 +1,54 @@
 """Консольный файловый менеджер.
 
 Основной модуль: точка входа."""
+
+import data
+import functions
+
+
+def main():
+    print(f'\n{data.TITLE}')
+    while True:
+        functions.show_menu()
+        choice = functions.get_menu_entry()
+
+        if choice is data.MainMenu.ADD:
+            pass
+
+        elif choice is data.MainMenu.COPY:
+            pass
+
+        elif choice is data.MainMenu.DEL:
+            pass
+
+        elif choice is data.MainMenu.DIR:
+            pass
+
+        elif choice is data.MainMenu.DIR_D:
+            pass
+
+        elif choice is data.MainMenu.DIR_F:
+            pass
+
+        elif choice is data.MainMenu.CD:
+            pass
+
+        elif choice is data.MainMenu.OS:
+            pass
+
+        elif choice is data.MainMenu.AUTHOR:
+            pass
+
+        elif choice is data.MainMenu.BANK:
+            pass
+
+        elif choice is data.MainMenu.QUIZ:
+            pass
+
+        elif choice is data.MainMenu.QUIT:
+            break
+
+
+# точка входа
+if __name__ == '__main__':
+    main()
